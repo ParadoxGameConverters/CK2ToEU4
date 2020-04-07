7183e764eaccb8a7bd45003973fbe3ea5b228510
Ivo Elezovic <ivo@elezovic.su>
2020-04-07 23:15:35 +0200

    kitch

--- CK2ToEU4/Source/EU4World/Output/outVersion.cpp
+++ CK2ToEU4/Source/EU4World/Output/outVersion.cpp
@@ -2,8 +2,11 @@
 
 std::ostream& mappers::operator<<(std::ostream& output, const VersionParser& versionParser)
 {
-	output << "Converter version " << versionParser.version << " \"" << versionParser.name << "\"\n";
-	output << versionParser.descriptionLine << "\n";
-	output << "Built on " << __TIMESTAMP__ << "\n";
+	output << "\n\n";
+	output << "************ -= The Paradox Converters Team =- ********************\n";
+	output << "* Converter version " << versionParser.version << " \"" << versionParser.name << "\"\n";
+	output << "* " << versionParser.descriptionLine << "\n";
+	output << "* Built on " << __TIMESTAMP__ << "\n";
+	output << "********************** + CK2 To EU4 + *****************************\n";
 	return output;
 }
