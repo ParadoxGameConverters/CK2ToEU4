186f1ccd8aa50d3cc0f1ad0f28e5fb73db853b78
Ivo Elezovic <ivo@elezovic.su>
2020-04-28 16:24:50 +0200

    adding config example for manual conversions

--- CK2ToEU4/Copy_Files.bat
+++ CK2ToEU4/Copy_Files.bat
@@ -1,15 +1,17 @@
 echo on
 rem Copy converter data files
+copy "Data_Files\log.txt" "..\Release\CK2ToEU4\log.txt"
+copy "Data_Files\configuration-example.txt" "..\Release\CK2ToEU4\configuration-example.txt"
+
 mkdir "..\Release\Configuration"
 copy "Data_Files\fronter-configuration.txt" "..\Release\Configuration\fronter-configuration.txt"
 copy "Data_Files\fronter-options.txt" "..\Release\Configuration\fronter-options.txt"
-copy "Data_Files\ReadMe.txt" "..\Release\readme.txt"
 
-copy "Data_Files\log.txt" "..\Release\CK2ToEU4\log.txt"
-copy "Data_Files\ReadMe.txt" "..\Release\CK2ToEU4\readme.txt"
-copy "Data_Files\FAQ.txt" "..\Release\CK2ToEU4\FAQ.txt"
-copy "Data_Files\license.txt" "..\Release\CK2ToEU4\license.txt"
-copy "Data_Files\after_converting.txt" "..\Release\CK2ToEU4\after_converting.txt"
+mkdir "..\Release\Docs"
+copy "Data_Files\ReadMe.txt" "..\Release\Docs\readme.txt"
+copy "Data_Files\FAQ.txt" "..\Release\Docs\FAQ.txt"
+copy "Data_Files\license.txt" "..\Release\Docs\license.txt"
+copy "Data_Files\after_converting.txt" "..\Release\Docs\after_converting.txt"
 
 rem Create Configurables
 del "..\Release\CK2ToEU4\configurables" /Q
@@ -20,8 +22,3 @@ rem Create Blank Mod
 del "..\Release\CK2ToEU4\blankMod" /Q
 rmdir "..\Release\CK2ToEU4\blankMod" /S /Q
 xcopy "Data_Files\blankMod" "..\Release\CK2ToEU4\blankMod" /Y /E /I
-
-rem Copy Flags
-rem del "..\Release\CK2ToEU4\flags" /Q
-rem rmdir "..\Release\CK2ToEU4\flags" /S /Q
-rem xcopy "Data_Files\flags" "..\Release\CK2ToEU4\flags" /Y /E /I
