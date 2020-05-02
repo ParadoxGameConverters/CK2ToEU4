f0b5c5126565eaef6501bcedd82af394ca338f2b
Ivo Elezović <ivo@elezovic.su>
2020-05-02 19:43:10 +0200

    Muslim dynasty renaming exclusions. (#104)

--- CK2ToEU4/Source/EU4World/Country/Country.cpp
+++ CK2ToEU4/Source/EU4World/Country/Country.cpp
@@ -256,8 +256,11 @@ void EU4::Country::initializeFromTitle(std::string theTag,
 	auto nameSet = false;
 	// Override for muslims kingdoms/empires.
 	std::set<std::string> muslimReligions = {"sunni", "zikri", "yazidi", "ibadi", "kharijite", "shiite", "druze", "hurufi", "qarmatian"};
-	if (muslimReligions.count(details.religion) && actualHolder->getDynasty().first && !actualHolder->getDynasty().second->getName().empty() &&
-		 title.first != "k_rum" && title.first != "k_israel" && title.first != "e_india" && (title.first.find("e_") == 0 || title.first.find("k_") == 0))
+	std::set<std::string> hardcodedExclusions =
+		 {"k_rum", "k_israel", "e_india", "e_il-khanate", "e_persia", "e_mali", "k_mali", "k_ghana", "k_songhay", "e_hre", "e_rome", "e_byzantium"};
+	if (details.government == "monarchy" && muslimReligions.count(details.religion) && actualHolder->getDynasty().first &&
+		 !actualHolder->getDynasty().second->getName().empty() && !hardcodedExclusions.count(title.first) &&
+		 (title.first.find("e_") == 0 || title.first.find("k_") == 0))
 	{
 		const auto& dynastyName = actualHolder->getDynasty().second->getName();
 		mappers::LocBlock newblock;
