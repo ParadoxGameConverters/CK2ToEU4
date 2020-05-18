1c2abc038959f17d20211058714db85779a347eb
Ivo Elezovic <ivo@elezovic.su>
2020-05-18 20:48:47 +0200

    integration and travis

--- CK2ToEU4/Copy_Files.sh
+++ CK2ToEU4/Copy_Files.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Copy converter globals
-cp "Data_Files/ReadMe.txt" "Release-Linux/ReadMe.txt"
-cp "Data_Files/FAQ.txt" "Release-Linux/FAQ.txt"
-cp "Data_Files/after_converting.txt" "Release-Linux/after_converting.txt"
-cp "Data_Files/license.txt" "Release-Linux/license.txt"
+cp "Data_Files/ReadMe.txt" "Release-Linux/"
+cp "Data_Files/FAQ.txt" "Release-Linux/"
+cp "Data_Files/after_converting.txt" "Release-Linux/"
+cp "Data_Files/license.txt" "Release-Linux/"
 
 # Copy configurables
 rm -rf "Release-Linux/configurables"
-cp -r "Data_Files/configurables" "Release-Linux/configurables"
+cp -r "Data_Files/configurables" "Release-Linux/"
+
+# Copy blankmod
+rm -rf "Release-Linux/blankMod"
+cp -r "Data_Files/blankMod" "Release-Linux/"
