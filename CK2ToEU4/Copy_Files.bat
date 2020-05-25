0db76fc5e71b08ad3831e80bdcd1c7eee31ec105
Ivo Elezović <ivo@elezovic.su>
2020-05-26 00:54:00 +0200

    Fronter, commons and x64 (#122)

--- CK2ToEU4/Copy_Files.bat
+++ CK2ToEU4/Copy_Files.bat
@@ -2,6 +2,9 @@ echo on
 rem Copy converter data files
 copy "Data_Files\log.txt" "..\Release\CK2ToEU4\"
 copy "Data_Files\configuration-example.txt" "..\Release\CK2ToEU4\"
+copy "Resources\msvcp140_codecvt_ids.dll" "..\Release\CK2ToEU4\"
+copy "Resources\vcruntime140_1.dll" "..\Release\CK2ToEU4\"
+copy "Resources\VC_redist.x64.exe" "..\Release\CK2ToEU4\"
 
 mkdir "..\Release\Configuration"
 copy "Data_Files\fronter-configuration.txt" "..\Release\Configuration\"
