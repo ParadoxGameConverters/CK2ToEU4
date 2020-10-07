e965a83f305b168fcb1984511fdc7b342b1b9560
Ivo Elezović <ivo@elezovic.su>
2020-10-07 14:28:21 +0200

    cleaning up solution and debug builds (#212)

--- CK2ToEU4/Copy_Files_Debug.bat
+++ CK2ToEU4/Copy_Files_Debug.bat
@@ -1,13 +1,20 @@
 echo on
 rem Copy converter data files
-copy "Data_Files\configuration.txt" "..\Debug\CK2ToEU4\configuration.txt"
-copy "Data_Files\ReadMe.txt" "..\Debug\CK2ToEU4\readme.txt"
-copy "Data_Files\ReadMe.txt" "..\Debug\readme.txt"
-copy "Data_Files\FAQ.txt" "..\Debug\CK2ToEU4\FAQ.txt"
-copy "Data_Files\CK2ToEU4DefaultConfiguration.xml" "..\Debug\Configuration\CK2ToEU4DefaultConfiguration.xml"
-copy "Data_Files\SupportedConvertersDefault.xml" "..\Debug\Configuration\SupportedConvertersDefault.xml"
-copy "Data_Files\license.txt" "..\Debug\CK2ToEU4\license.txt"
-copy "Data_Files\after_converting.txt" "..\Debug\CK2ToEU4\after_converting.txt"
+copy "Data_Files\log.txt" "..\Debug\CK2ToEU4\"
+copy "Data_Files\configuration-example.txt" "..\Debug\CK2ToEU4\"
+copy "Resources\msvcp140_codecvt_ids.dll" "..\Debug\CK2ToEU4\"
+copy "Resources\vcruntime140_1.dll" "..\Debug\CK2ToEU4\"
+
+mkdir "..\Debug\Configuration"
+copy "Data_Files\fronter-configuration.txt" "..\Debug\Configuration\"
+copy "Data_Files\fronter-options.txt" "..\Debug\Configuration\"
+copy "Data_Files\*.yml" "..\Debug\Configuration\"
+
+mkdir "..\Debug\Docs"
+copy "Data_Files\ReadMe.txt" "..\Debug\Docs\"
+copy "Data_Files\FAQ.txt" "..\Debug\Docs\"
+copy "Data_Files\license.txt" "..\Debug\Docs\"
+copy "Data_Files\after_converting.txt" "..\Debug\Docs\"
 
 rem Create Configurables
 del "..\Debug\CK2ToEU4\configurables" /Q
@@ -15,11 +22,6 @@ rmdir "..\Debug\CK2ToEU4\configurables" /S /Q
 xcopy "Data_Files\configurables" "..\Debug\CK2ToEU4\configurables" /Y /E /I
 
 rem Create Blank Mod
-rem del "..\Debug\CK2ToEU4\blankMod" /Q
-rem rmdir "..\Debug\CK2ToEU4\blankMod" /S /Q
-rem xcopy "Data_Files\blankMod" "..\Debug\CK2ToEU4\blankMod" /Y /E /I
-
-rem Copy Flags
-rem del "..\Debug\CK2ToEU4\flags" /Q
-rem rmdir "..\Debug\CK2ToEU4\flags" /S /Q
-rem xcopy "Data_Files\flags" "..\Debug\CK2ToEU4\flags" /Y /E /I
+del "..\Debug\CK2ToEU4\blankMod" /Q
+rmdir "..\Debug\CK2ToEU4\blankMod" /S /Q
+xcopy "Data_Files\blankMod" "..\Debug\CK2ToEU4\blankMod" /Y /E /I
