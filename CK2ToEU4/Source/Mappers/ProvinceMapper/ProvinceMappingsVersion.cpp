e4723ea0a4f46e39b4ffbf47896b90d9a7991e1d
Zemurin <ivo@elezovic.su>
2020-10-21 14:52:42 +0200

    provinces to new format

--- CK2ToEU4/Source/Mappers/ProvinceMapper/ProvinceMappingsVersion.cpp
+++ CK2ToEU4/Source/Mappers/ProvinceMapper/ProvinceMappingsVersion.cpp
@@ -5,9 +5,11 @@ mappers::ProvinceMappingsVersion::ProvinceMappingsVersion(std::istream& theStrea
 {
 	registerKeyword("link", [this](const std::string& unused, std::istream& theStream) {
 		const ProvinceMapping newMapping(theStream);
+		if (newMapping.getCK2Provinces().empty() && newMapping.getEU4Provinces().empty())
+			return;
 		mappings.push_back(newMapping);
 	});
-	registerRegex("[a-zA-Z0-9\\_.:-]+", commonItems::ignoreItem);
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 
 	parseStream(theStream);
 	clearRegisteredKeywords();
