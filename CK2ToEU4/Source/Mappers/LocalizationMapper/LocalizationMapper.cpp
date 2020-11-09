81b40469935f2f93a1e3a1741586c2797af76e0b
Ivo Elezovic <ivo@elezovic.su>
2020-11-09 08:43:31 +0100

    indented too.

--- CK2ToEU4/Source/Mappers/LocalizationMapper/LocalizationMapper.cpp
+++ CK2ToEU4/Source/Mappers/LocalizationMapper/LocalizationMapper.cpp
@@ -48,7 +48,7 @@ void mappers::LocalizationMapper::scrapeStream(std::istream& theStream)
 		std::string line;
 		getline(theStream, line);
 
-		if (line.length() < 4 || line [0] == '#')
+		if (line.length() < 4 || line [0] == '#' || line[1] == '#' )
 			continue;
 
 		auto sepLoc = line.find_first_of(';');
