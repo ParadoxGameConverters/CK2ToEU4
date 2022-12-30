b07e9806aa1d020b8fe475867ca08e7314f7e350
កាបូន26 <83087301+CarbonY26@users.noreply.github.com>
2022-12-30 00:27:20 -0600

    Mapping e_china

--- CK2ToEU4/Source/EU4World/EU4World.cpp
+++ CK2ToEU4/Source/EU4World/EU4World.cpp
@@ -755,6 +755,8 @@ void EU4::World::adjustChina(const CK2::World& sourceWorld, const Configuration:
 	for (const auto& country: countries)
 	{
 		if (country.second->getTitle().first != "e_china_west_governor")
+		else if (mod.name == "Tianxia: Silk Road Expansion")
+			(country.second->getTitle().first != "e_china")
 			continue;
 		const auto& westernTag = country.first;
 		// Move our diplo to China
