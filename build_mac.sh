415b0b2c6be64ae8e4d667de61f3c9d4c073c77e
klorpa <30924131+klorpa@users.noreply.github.com>
2023-01-22 20:30:29 -0600

    TrailingSpaces

--- build_mac.sh
+++ build_mac.sh
@@ -4,6 +4,6 @@ rm -rf build
 rm -rf Release-Linux
 rm -rf ../Release
 cmake -H. -Bbuild -DCMAKE_C_COMPILER=/usr/local/bin/gcc-9 -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-9 -DCMAKE_EXE_LINKER_FLAGS=-liconv
-cmake --build build -- -j3 
+cmake --build build -- -j3
 mv Release-Linux ../Release
-cd .. 
+cd ..
