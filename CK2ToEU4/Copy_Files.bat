d726a9e95e4a11b063ab9782f53d4af75c0dbf13
Ivo Elezovic <ivo.elezovic@gmail.com>
2023-01-23 13:45:56 +0100

    ditch excess files

--- CK2ToEU4/Copy_Files.bat
+++ CK2ToEU4/Copy_Files.bat
@@ -4,6 +4,7 @@ copy "Data_Files\log.txt" "..\Release\CK2ToEU4\"
 copy "Data_Files\configuration-example.txt" "..\Release\CK2ToEU4\"
 copy "Resources\msvcp140_codecvt_ids.dll" "..\Release\CK2ToEU4\"
 copy "Resources\vcruntime140_1.dll" "..\Release\CK2ToEU4\"
+copy "Data_Files\GeneralFAQ-READ.ME.FIRST.txt" "..\Release\"
 
 mkdir "..\Release\Configuration"
 copy "Data_Files\fronter-configuration.txt" "..\Release\Configuration\"
