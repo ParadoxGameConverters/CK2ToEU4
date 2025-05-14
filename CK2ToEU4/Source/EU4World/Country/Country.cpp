c3c2ffc0d84222ff7f3f04c90258f1323cd28fc9
Ivo Elezovic <ivo.elezovic@gmail.com>
2025-05-14 14:20:58 +0200

    cmath

--- CK2ToEU4/Source/EU4World/Country/Country.cpp
+++ CK2ToEU4/Source/EU4World/Country/Country.cpp
@@ -13,6 +13,7 @@
 #include "../Province/EU4Province.h"
 #include "CommonFunctions.h"
 #include "Log.h"
+#include "cmath"
 
 EU4::Country::Country(std::string theTag, const std::filesystem::path& filePath): tag(std::move(theTag))
 {
